dc1d8dca2da46958c85a326121e593df11b64c16
User <jliveo@gmail.com>
2025-11-09 22:12:55 +0900

    test

---
+++ ep04.py
@@ -0,0 +1,49 @@
+# 데이터 타입과 구조
+# 정수 -> 소수점이 없는 숫자
+# age = 20
+# print(age)
+# print(type(age))
+# # 실수 -> 소수점이 있는 숫자
+# weight = 80.5
+# print(weight)
+# print(type(weight))
+# # 문자열 - > 문자
+# greeting = "Hello World"
+# print(greeting)
+# print(type(greeting))
+#
+# # 불리언
+# is_adult = True
+# print(is_adult)
+# print(type(is_adult))
+#--------------------------
+# 파이썬의 자료구조
+
+# 리스트
+# 여러 요소를 저장할 때 많이 사용되는 자료구조
+# fruits = [1,2,"딸기"]
+# print(fruits)
+# print(type(fruits))
+#
+# 튜플
+# 한번 값이 초기화에 설정(변하지 않는 값을 저장할 때 사용)
+# location = ("37.123", "126.957")
+# print(location)
+# print(type(location))
+#
+# 딕셔너리
+# student_scores={"철수":90,"영희":85,"민수":95}
+# # 중괄호 key값가 value 등 요소를 기재
+# # 딕셔너리의 요소(key, value)
+# print(student_scores)
+# print(type(student_scores))
+
+# set (중복을 허용하지 않음)
+colors = {'red', 'red', 'green', 'blue', 'yellow', 'magenta', 'cyan', 'white'}
+print(colors)
+
+
+
+
+
+
