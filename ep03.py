5ad184e3a3cd16e1b7931e3358307c2e403a9247
User <jliveo@gmail.com>
2025-11-09 21:47:46 +0900

    test

---
+++ ep03.py
@@ -0,0 +1,21 @@
+# #range 함수 for문
+# for num in range(1,101):
+# for문 끝에 콜론 사용
+#     print(num)
+#     # for문에서 엔트 시 앞에 띄워쓰기나는 것이 인덴트
+#     # 드래그 선택 후 ctrl+/ 코멘트화(주석처리)
+#
+# print("end")
+
+# # list 함수 for문
+# fruits = ["사과","바나나","딸기"]
+# 리스트는 대괄호를 사용
+# for i in fruits:
+#     print(i)
+
+# dictionary for문
+# student_scores = {"철수":90,"영희":85,"민수":95}
+# for name, score in student_scores.items():
+#     print(name, score)
+
+
